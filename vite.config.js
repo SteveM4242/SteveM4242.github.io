dd2307f2547c8dfad5697fe472db5cc2a48172d1
SteveM42 <leonhard.ziegler@outlook.de>
2026-02-10 21:08:46 +0100

    Finished Vite Setup

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Wenn es ein USERNAME.github.io Repo ist, base = '/'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/' // für USERNAME.github.io
+  base: '/SteveM42.github.io'
 })
