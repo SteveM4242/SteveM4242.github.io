9f5c1c2d35011a0abd2e9e9ca61cfb04c46872ea
SteveM42 <leonhard.ziegler@outlook.de>
2026-02-10 21:05:09 +0100

    deploy gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Wenn es ein USERNAME.github.io Repo ist, base = '/'
 export default defineConfig({
   plugins: [react()],
+  base: '/' // für USERNAME.github.io
 })
